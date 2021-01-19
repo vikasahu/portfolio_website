ef7edde36a076669192cad2bd66b60711db0de7b
vikasahu <vikas.sahu@rentomojo.com>
2021-01-19 21:55:17 +0530

    added git ignore

--- .nuxt/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Logo } from '../../components/Logo.vue'
-export { default as WelcomeLayout } from '../../components/Wrapper/WelcomeLayout.vue'
-
-export const LazyLogo = import('../../components/Logo.vue' /* webpackChunkName: "components/Logo" */).then(c => c.default || c)
-export const LazyWelcomeLayout = import('../../components/Wrapper/WelcomeLayout.vue' /* webpackChunkName: "components/Wrapper/WelcomeLayout" */).then(c => c.default || c)
